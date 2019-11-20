4bf3174e1e1adc3d86a75181ac7520c833672128
koennjb <koennjbecker@gmail.com>
2019-11-20 12:04:17 -0800

    Created an abstract AuthService It should be implemented by new authentication providers

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,18 @@
+import 'package:journal/models/user.dart';
+
+/**
+ * Abstract service which contains all functionality of an authentication service.
+ * Provide implementations of this class for each different authentication provider you need
+ */
+abstract class AuthService {
+  Future<User> currentUser();
+  Future<User> signInAnonymously();
+  Future<User> signInWithEmailAndPassword(String email, String password);
+  Future<User> createUserWithEmailAndPassword(String email, String password);
+  Future<void> sendPasswordResetEmail(String email);
+  Future<User> signInWithGoogle();
+  Future<User> signInWithFacebook();
+  Future<void> signOut();
+  Stream<User> get onAuthStateChanged;
+  void dispose();
+}
